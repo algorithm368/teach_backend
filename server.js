47a2e5aaf549de7ac41f69222eec7e8077b89ce1
algorithm368 <earthchairuk@gmail.com>
2024-06-06 23:24:35 +0700

    add new database

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const mysql = require('mysql');
 const port = 8000;
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'mysql_nodejs',
-    password: '',
+    host: 'sql12.freesqldatabase.com',
+    user: 'sql12712112',
+    database: 'sql12712112',
+    password: 'fE5j68QzaX',
     port: '3306'
 });
 
